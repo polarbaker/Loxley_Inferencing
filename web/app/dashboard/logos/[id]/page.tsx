1f51bc3a6b396bb58ca3f2d81efdc891172a96d5
Jack O'Brien <thejackobrien29@gmail.com>
2024-11-17 18:17:48 -0500

    abilit to edit prompts (kinda)

--- web/app/dashboard/logos/[id]/page.tsx
+++ web/app/dashboard/logos/[id]/page.tsx
@@ -2,11 +2,13 @@
 import { useForm } from 'react-hook-form';
 import * as yup from 'yup';
 import { yupResolver } from '@hookform/resolvers/yup';
-import { useQuery } from 'convex/react';
+import { useMutation, useQuery } from 'convex/react';
 import { api } from '@/convex/_generated/api';
 import { useParams } from 'next/navigation';
 import { Id } from '@/convex/_generated/dataModel';
 import { Button } from '@/components/ui/button';
+import { Textarea } from '@/components/ui/textarea';
+import { Badge } from '@/components/ui/badge';
 
 export default function Dashboard() {
   const { id } = useParams();
@@ -14,6 +16,8 @@ export default function Dashboard() {
   const logoSet = useQuery(api.logos.getLogoSet, {
     logoSetId: id as Id<'logoSets'>,
   });
+
+  const updateLogoPrompt = useMutation(api.logos.updateLogoPrompt);
 
   return (
     <div className="flex flex-1 flex-col gap-4 p-4">
@@ -40,11 +44,25 @@ export default function Dashboard() {
         </div>
 
         <div className="rounded-lg border p-4 mt-8">
-          <h2 className="text-lg font-semibold mb-4">All Prompts</h2>
+          <h2 className="text-lg font-semibold mb-4 flex items-center gap-2">
+            All Prompts <Badge variant="outline">{logoSet?.logos.length}</Badge>
+          </h2>
+          <div className="text-sm text-muted-foreground mb-4">
+            Updating the prompts will save immediately
+          </div>
           <div className="flex flex-col gap-2">
             {logoSet?.logos.map((logo, index) => (
-              <div key={index} className="text-sm text-muted-foreground">
-                {logo.prompt}
+              <div key={index} className="flex flex-col gap-2">
+                <div className="text-sm font-medium">Prompt #{index + 1}</div>
+                <Textarea
+                  key={index}
+                  value={logo.prompt}
+                  className="text-sm"
+                  onChange={(e) => updateLogoPrompt({ logoId: logo._id, prompt: e.target.value })}
+                />
+                {index < logoSet.logos.length - 1 && (
+                  <hr className="border-t border-gray-200 my-2" />
+                )}
               </div>
             ))}
           </div>
